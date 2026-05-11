7941adb981f26cc5dfebe20111afd59702bc1860
Alysha-Braafhart <236565157+Alysha-Braafhart@users.noreply.github.com>
2026-05-11 15:01:02 +0200

    Player Movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   public int speed = 5;
     void Start()
     {
         
@@ -11,6 +11,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0) * Time.deltaTime * speed;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0) * Time.deltaTime * speed;
+        } 
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * speed;
+        }
+    }  
 }
