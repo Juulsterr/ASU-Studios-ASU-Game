69a833c61ef3a50f021376b5c290fe6b3b0b2e27
Julian <40456@ma-web.nl>
2026-05-12 13:11:26 +0200

    day2

--- Assets/Script/PlayerMMovement.cs
+++ Assets/Script/PlayerMMovement.cs
@@ -1,33 +1,37 @@
 using UnityEngine;
-
-public class Enemy : MonoBehaviour
+ 
+public class PlayerMMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed = 2.5f;
-    
-
+    public float sprintSpeed = 5f;
+ 
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Vector2 movement;
-
+ 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+ 
     void Update()
     {
         // Input ophalen
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
-
+ 
         // Zorgt dat diagonaal niet sneller gaat
         moveInput = moveInput.normalized;
-
-       
-
+ 
+        // Sprint check
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift)
+            ? sprintSpeed
+            : moveSpeed;
+ 
+        movement = moveInput * currentSpeed;
     }
-
+ 
     void FixedUpdate()
     {
         // Smooth movement
