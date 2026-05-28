716855b532464da0394c136ab52430e8964b8d71
Alysha-Braafhart <236565157+Alysha-Braafhart@users.noreply.github.com>
2026-05-28 10:07:10 +0200

    I DID IT!

--- Assets/Script/ShootLazers.cs
+++ Assets/Script/ShootLazers.cs
@@ -4,22 +4,36 @@ using System.Collections.Generic;
 
 public class ShootLazers : MonoBehaviour
 {
+    [SerializeField]
+ 
+    private bool canShoot = true;
     public Transform firePoint;
     public GameObject lazer;
     
     // Update is called once per frame
-    void Update()
+     private void Update()
     {
-       if (Input.GetMouseButtonDown(0))
+        
+       if (Input.GetMouseButtonDown(0)&& canShoot)
         {
+            StartCoroutine(ShootCooldown());
             Shoot();
         }
     }
+
+    
 
     void Shoot()
     {
       Instantiate(lazer, firePoint.position, firePoint.rotation); 
     }
 
-    
+    private System.Collections.IEnumerator ShootCooldown()
+    {
+        canShoot = false;
+ 
+        yield return new WaitForSeconds(1);
+ 
+        canShoot = true;
+    }
 }
