924291bb52c2c2f513ce15ec95909e66cd08e2b7
Alysha-Braafhart <236565157+Alysha-Braafhart@users.noreply.github.com>
2026-06-02 11:54:03 +0200

    ALLEEN DEZE BRANCH!!!!

--- Assets/Script/Bar.cs
+++ Assets/Script/Bar.cs
@@ -33,7 +33,7 @@ public class Bar : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("LightLazer"))
         {
             TakeDamage(25);
             Debug.Log("Enemy geraakt");
