02a35426e2392f7a86f1cec6cb7bda4f163934d1
Alysha-Braafhart <236565157+Alysha-Braafhart@users.noreply.github.com>
2026-06-09 13:00:51 +0200

    Boss

---
+++ Assets/Script/SwordSlash.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SwordSlash : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 20f;
+    public int damage = 34;
+
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.linearVelocity = transform.right * speed;
+       Destroy(gameObject, 3f);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+    
+
+    void Update()
+    {
+        
+    }
+}
